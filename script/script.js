7fcfe5ecc35e5eb9554e3e60aab69220240a3413
San4es <mister.San4es@list.ru>
2021-02-15 18:54:00 +0400

    lesson12

--- script/script.js
+++ script/script.js
@@ -1,2 +1,10 @@
 'use strict';
 
+const todoControl = document.querySelector('todo-control'),
+  headerInput = document.querySelector('header-input'),
+  todoList = document.querySelector('todo-list'),
+  todoCompleted = document.querySelector('todo-completed');
+
+  const todoData = [
+    
+  ]
