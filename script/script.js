3474870c91b71623e8a27482656c67e340fa464a
San4es <mister.San4es@list.ru>
2021-02-25 20:46:00 +0400

    lesson15.2

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
 'use strict';
 
 class First {
-  constructor() {
-
-  }
   hello() {
     console.log("Привет я метод родителя!");
   }
 }
 
-class Second {
-  constructor() {
+const first = new First();
 
-  }
+class Second extends First {
   hello() {
+    super.hello();
     console.log("А я наследуемый метод!");
   }
 }
+
+const second = new Second();
+second.hello();
